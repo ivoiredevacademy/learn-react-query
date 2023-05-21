66028526e4ccb81edd8251e935b3f527580801a2
Juvenal PENGELE <juvenal.pengele@gmail.com>
2023-05-21 11:25:28 +0000

    Integrate users fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,47 @@ import { Post } from "../components/Posts/Post";
 
 export function Home() {
   return (
-    <div className="flex flex-wrap -mx-8 py-10">
-      <div className="w-1/3 px-8">
-        <Post title="Article" description="Description" />
+    <>
+    <h1 className="text-5xl text-center my-10">Articles</h1>
+     <div className="flex flex-wrap -mx-8 py-10">
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
       </div>
-
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
+      </div>
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
+      </div>
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
+      </div>
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
+      </div>
+      <div className="w-1/3 px-8 my-8">
+        <Post
+          title="Lorem ipsum dolor sit"
+          description="Deserunt similique assumenda saepe ipsum nobis quos cupiditate excepturi ab atque fugit facilis deleniti eveniet veniam nostrum harum, provident esse autem sunt?"
+        />
+      </div>
     </div>
+    </>
+
   )
 }
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function Users() {
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    async function fetchAuthors() {
+      setIsLoading(true);
+      try {
+        const response = await axios.get('http://localhost:3000/users');
+        setAuthors(response.data)
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchAuthors();
+  }, []);
+
   return (
     <div>
-      Users
+      <h1 className="text-4xl my-8 text-center">Auteurs</h1>
+      <ul>
+        {
+          authors.map((author) => (
+            <li key={author.id} className="flex flex-col my-4">
+              <span>{author.name} <span className="text-gray-400">@{author.username}</span></span>
+              <span className="text-gray-500">{author.email}</span>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
