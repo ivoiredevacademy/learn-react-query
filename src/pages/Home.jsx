b5253d6826203adf656aca6a30ed7a3a60cd3799
Juvenal PENGELE <juvenal.pengele@gmail.com>
2023-05-28 10:02:54 +0000

    08 - Manually fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
 import axios from "axios";
 import { Post } from "../components/Posts/Post";
-import { Spinner } from '../components/layouts/Spinner';
+import { useState } from "react";
 import { useQuery } from '@tanstack/react-query';
 import { useAuthors } from "../hooks/useAuthors";
+
 
 const truncate = (str, length = 20) => str.substr(0, length, ) + '...';
 
 export function Home() {
-
-  const result = useQuery({
+  const [searchText, setSearchText] = useState('');
+  const { error, data, status, isError, refetch } = useQuery({
     queryKey: ['posts'],
     queryFn: async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000), )
-      return axios.get('http://localhost:3000/posts?_limit=12&page=1')
+      const url = `http://localhost:3000/posts?_limit=12&page=1&title_like=${searchText}`
+      return axios.get(url)
     },
+    cacheTime: 30000,
   })
 
   const { data: authors } = useAuthors();
@@ -22,20 +24,32 @@ export function Home() {
     return authors?.data?.find((author) => author.id === post.userId)?.name || "Anonyme";
   }
 
-  if(result.status === 'loading') {
+  if(status === 'loading') {
     return <div className="text-gray-500 text-center text-xl my-4">Loading...</div>
   }
 
-  if(result.isError) {
-    return <div className="text-red-500 text-center text-xl my-4">{result.error.message}</div>
+  if(isError) {
+    return <div className="text-red-500 text-center text-xl my-4">{error.message}</div>
+  }
+
+  function handleKeyPressed(e) {
+    if(e.key === 'Enter') {
+      refetch();
+    }
   }
 
   return (
     <>
-      <h1 className="text-5xl text-center my-10">Articles</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-5xl text-center my-10">Articles</h1>
+        <input type="text" className="input" onChange={(e) => setSearchText(e.target.value)}
+          onKeyDown={handleKeyPressed}
+        />
+      </div>
+
       <div className="flex flex-wrap -mx-8 py-10">
         {
-          result.data?.data.map((post) => (
+          data?.data.map((post) => (
             <div className="w-1/3 px-8 my-8" key={post.id}>
               <Post
                 title={truncate(post.title, 70)}
