0db9a41bf25556692f28d9497fc8e75662cfe456
Juvenal PENGELE <juvenal.pengele@gmail.com>
2023-06-03 10:13:01 +0000

    08 - Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,15 @@ export function Home() {
   const { error, data, status, isError, refetch } = useQuery({
     queryKey: ['posts'],
     queryFn: async () => {
-      const url = `http://localhost:3000/posts?_limit=12&page=1&title_like=${searchText}`
+      let url = `http://localhost:3000/posts?_limit=15&_page=${page}`;
+      if(inputRef?.current?.value) {
+        url += `&title_like=${inputRef?.current?.value}`;
+      }
       return axios.get(url)
     },
     cacheTime: 30000,
+    keepPreviousData: true,
+    retry: 1
   })
 
   const { data: authors } = useAuthors();
@@ -32,18 +37,45 @@ export function Home() {
     return <div className="text-red-500 text-center text-xl my-4">{error.message}</div>
   }
 
-  function handleKeyPressed(e) {
-    if(e.key === 'Enter') {
+   function fetchMoreArticles() {
+    setPage((page) => page + 1);
+    window.scrollTo({
+      top: 0
+    })
+  }
+
+  function fetchPreviousArticles() {
+    setPage((page) => page - 1);
+    window.scrollTo({
+      top: 100
+    })
+  }
+
+  function handleSearchChange(event) {
+    if(inputRef?.current?.value.length === 0) {
+      refetch();
+      return;
+    }
+
+    if(event.key === 'Enter' && inputRef.current.value.length > 3 ) {
       refetch();
     }
   }
+
+
+  const hasNextPages = posts?.data?.length === 15;
+  const hasPreviousPages = page > 1;
 
   return (
     <>
       <div className="flex justify-between items-center">
         <h1 className="text-5xl text-center my-10">Articles</h1>
-        <input type="text" className="input" onChange={(e) => setSearchText(e.target.value)}
-          onKeyDown={handleKeyPressed}
+         <input
+          type="text"
+          className="input"
+          placeholder="Search articles..."
+          ref={inputRef}
+          onKeyDown={handleSearchChange}
         />
       </div>
 
@@ -62,6 +94,28 @@ export function Home() {
           ))
         }
       </div>
+       <div className="w-full flex justify-between my-10">
+          <button
+            disabled={!hasPreviousPages}
+            className="px-4 py-2 border flex rounded border-gray-500 text-gray-700"
+            onClick={fetchPreviousArticles}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+            </svg>
+            Previous articles
+          </button>
+          <button
+            disabled={!hasNextPages}
+            className="px-4 flex py-2 border rounded border-gray-500 text-gray-700"
+            onClick={fetchMoreArticles}
+          >
+            Next page
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+            </svg>
+          </button>
+        </div>
     </>
   )
 }
